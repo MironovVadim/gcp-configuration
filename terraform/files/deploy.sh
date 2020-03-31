13dee04db5fd02b75d029a2c9851e55a03805669
Mironov_vn <vmironov@roox.ru>
2020-03-31 18:27:38 +0300

    terraform done

--- terraform/files/deploy.sh
+++ terraform/files/deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 set -e
-
 source ~/.profile
 git clone https://github.com/Artemmkin/reddit.git
 cd reddit
 bundle install
 
 sudo mv /tmp/puma.service /etc/systemd/system/puma.service
-sudo systemctl start puma
 sudo systemctl enable puma
+sudo systemctl start puma
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,59 +1,59 @@
 provider "google" {
-    project = "terraform-project-272717"
-        region = "europe-west1"
+  project = var.project
+  region  = var.region
 }
 
 resource "google_compute_instance" "app" {
-    name = "reddit-app"
-        machine_type = "g1-small"
-        zone = "europe-west1-b"
-         # определение загрузочного диска
-        boot_disk {
-        initialize_params {
-            image = "reddit-base-1585613915"
-        }
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "europe-west1-b"
+  # определение загрузочного диска
+  boot_disk {
+    initialize_params {
+      image = var.disk_image
     }
+  }
 
-    metadata = {
-        sshKeys = "appuser:${file("~/.ssh/appuser.pub")}"		
-    }
+  metadata = {
+    sshKeys = "appuser:${file(var.public_key_path)}"
+  }
 
-    tags = ["puma-server"]
-     # определение сетевого интерфейса
-    network_interface {
-		network = "default"
-         # сеть, к которой присоединить данный интерфейс network = "default"
-             # использовать ephemeral IP для доступа из Интернет
-        access_config {}
-    }
-	
-	connection {
-		host = self.network_interface.0.access_config.0.nat_ip
-		type = "ssh"
-		user = "appuser"
-		agent = false
-		private_key = "${file("~/.ssh/appuser")}"
-	}
-	provisioner "file" {
-		source = "files/puma.service"
-		destination = "/tmp/puma.service"
-	}
-	provisioner "remote-exec" {
-		script = "files/deploy.sh"
-	}
+  tags = ["puma-server"]
+  # определение сетевого интерфейса
+  network_interface {
+    network = "default"
+    # сеть, к которой присоединить данный интерфейс network = "default"
+    # использовать ephemeral IP для доступа из Интернет
+    access_config {}
+  }
+
+  connection {
+    host        = self.network_interface.0.access_config.0.nat_ip
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = file(var.private_key_path)
+  }
+  provisioner "file" {
+    source      = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
 }
 
 resource "google_compute_firewall" "firewall_puma" {
-	name = "allow-puma-default"
-	# Название сети, в которой действует правило
-	network = "default"
-	# Какой доступ разрешить
-	allow {
-		protocol = "tcp"
-		ports = ["9292"]
-	}
-	# Каким адресам разрешаем доступ
-	source_ranges = ["0.0.0.0/0"]
-	# Правило применимо для инстансов с тегом …
-	target_tags = ["puma-server"]
+  name = "allow-puma-default"
+  # Название сети, в которой действует правило
+  network = "default"
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+  # Правило применимо для инстансов с тегом …
+  target_tags = ["puma-server"]
 }
