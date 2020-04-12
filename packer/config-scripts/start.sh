17f5e85eaa79e63dd5410821f6dd0dddbf6cc38d
Mironov_vn <vmironov@roox.ru>
2020-04-13 00:13:03 +0300

    ansible

--- packer/config-scripts/start.sh
+++ packer/config-scripts/start.sh
@@ -3,4 +3,5 @@
 git clone https://github.com/MironovVadim/gcp-configuration.git
 cd gcp-configuration/config-scripts
 (. install_ruby.sh ;
-. install_mongodb.sh)
+. install_mongodb.sh ;
+. deploy.sh)
